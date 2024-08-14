6225a2427c265231280ccf10c15386d8b863f5b7
Koushik <banerjeekoushik04@gmail.com>
2024-08-14 11:30:58 +0530

    commit

---
+++ classees.js
@@ -0,0 +1,10 @@
+class person{
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const details = new person("Koushik",22);
+console.log(details.name);
+console.log(details.age);
