87a1082f195da5ce356b8cd9202314ce8183a77b
Koushik <banerjeekoushik04@gmail.com>
2024-08-14 11:52:10 +0530

    commit

--- classees.js
+++ classees.js
@@ -1,10 +1,33 @@
-class person{
-    constructor(name,age) {
-        this.name = name;
-        this.age = age;
+// class person{
+//     constructor(name,age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// const details = new person("Koushik",22);
+// console.log(details.name);
+// console.log(details.age);
+
+
+
+class Car{
+    constructor(brand,model,hp){
+        this.brand=brand;
+        this.model=model;
+        this.hp=hp;
+    }
+
+
+    display(){
+        console.log(this.brand);
+        console.log(this.model);
+        console.log(this.hp);
+        console.log();
     }
 }
 
-const details = new person("Koushik",22);
-console.log(details.name);
-console.log(details.age);
+const toyota = new Car("Toyota", "fortuner", "500hp")
+const ford = new Car("Ford","Mustang", "750hp")
+toyota.display()
+ford.display()
