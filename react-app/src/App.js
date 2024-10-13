b884f8ac994861f7d238a98ca76fdac1023322d5
Koushik <banerjeekoushik04@gmail.com>
2024-10-14 02:03:23 +0530

    adding object and fetching through map

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,58 @@
 import "./App.css";
 import Video from "./component/Video";
 function App() {
-  let obj = {
+  let obj = [{
+    id:1,
     title: "React js tutorial",
     views: "210k",
     time: "1 year ago",
     channel: "kbshorts",
     verified: false,
-  };
-  let channelName = "kbshorts";
+  },
+  {
+    id:2,
+    title: "Node js tutorial",
+    views: "500k",
+    time: "5 year ago",
+    channel: "kbshorts",
+    verified: true,
+  },
+  {
+    id:3,
+    title: "MongoDB tutorial",
+    views: "1M",
+    time: "6 months ago",
+    channel: "kbshorts",
+    verified: false,
+  },
+  {
+    id:4,
+    title: "Rust tutorial",
+    views: "1.5M",
+    time: "8 months ago",
+    channel: "kbshorts",
+    verified: true,
+  }];
+  // let channelName = "kbshorts";
   return (
     <div>
-      <div style={{ display: "flex" }}>Yt Shorts</div>
+      <div style={{ display: "flex" , justifyContent:"center"}}>Yt Shorts</div>
       <div className="playlist">
-        <Video {...obj}></Video>
-        <Video
+        {
+          obj.map(video=><Video
+            id={video.id}
+            title={video.title}
+            views={video.views}
+            time={video.time}
+            channel={video.channel}
+            verified={video.verified}
+          ></Video>)
+        }
+      
+
+
+        {/* <Video {...obj}></Video>
+         <Video
           title="Node js tutorial"
           views="500k"
           time="2 year ago"
@@ -34,10 +72,14 @@ function App() {
           time="2 year ago"
           channel={channelName}
           verified={true}
-        ></Video>
+        ></Video> */}
+
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
--- react-app/src/component/Video.js
+++ react-app/src/component/Video.js
@@ -1,5 +1,5 @@
 import "./Video.css";
-function Video({ title, channel = "kbshorts", views, time, verified }) {
+function Video({ id, title, channel = "kbshorts", views, time, verified }) {
   //puting a default value in prop
 
   // lengthy process for conditional
@@ -14,7 +14,7 @@ function Video({ title, channel = "kbshorts", views, time, verified }) {
     <>
       <div className="container">
         <div className="pic">
-          <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+          <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />   {/*use of string literal*/}
         </div>
         <div className="title">{title}</div>
         <div className="channel">
